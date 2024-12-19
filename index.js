114ffb731f968cf1b5c980cb402d7dac24175415
Paulo Almeida <paulloalmeida.84@gmail.com>
2024-12-19 20:57:38 -0300

    update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-vitorias = 10
+vitorias = 1065
 derrotas = 2
 let nivel
 
@@ -9,31 +9,31 @@ console.log(vitorias - derrotas)
 switch (true) {
     case saldoVitorias < 10:
         nivel = "Ferro";
-        console.log("O herói tem saldo de " + saldoVitorias() + "e está no nível " + nivel + "!");
+        console.log("O herói tem saldo de " + saldoVitorias() + " e está no nível " + nivel + "!");
         break;
     case saldoVitorias <= 20:
         nivel = "Bronze";
-        console.log("O herói tem saldo de " + saldoVitorias() + "e está no nível " + nivel + "!");
+        console.log("O herói tem saldo de " + saldoVitorias() + " e está no nível " + nivel + "!");
         break;
     case saldoVitorias <= 50:
         nivel = "Prata";
-        console.log("O herói tem saldo de " + saldoVitorias() + "e está no nível " + nivel + "!");
+        console.log("O herói tem saldo de " + saldoVitorias() + " e está no nível " + nivel + "!");
         break;
     case saldoVitorias <= 80:
         nivel = "Ouro";
-        console.log("O herói tem saldo de " + saldoVitorias() + "e está no nível " + nivel + "!");
+        console.log("O herói tem saldo de " + saldoVitorias() + " e está no nível " + nivel + "!");
         break;
     case saldoVitorias <= 90:
         nivel = "Diamante";
-        console.log("O herói tem saldo de " + saldoVitorias() + "e está no nível " + nivel + "!");
+        console.log("O herói tem saldo de " + saldoVitorias() + " e está no nível " + nivel + "!");
         break;
     case saldoVitorias <= 100:
         nivel = "Lendário";
-        console.log("O herói tem saldo de " + saldoVitorias() + "e está no nível " + nivel + "!");
+        console.log("O herói tem saldo de " + saldoVitorias() + " e está no nível " + nivel + "!");
         break;
     default:
         nivel = "Imortal";
-        console.log("O herói tem saldo de " + saldoVitorias() + "e está no nível " + nivel + "!");
+        console.log("O herói tem saldo de " + saldoVitorias() + " e está no nível " + nivel + "!");
         break;
     
 }
