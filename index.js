9c90a3439038e430c2fdec49b72383b4e4a6ca75
Paulo Almeida <paulloalmeida.84@gmail.com>
2024-12-24 14:46:55 -0300

    update index.js

--- index.js
+++ index.js
@@ -2,41 +2,34 @@
 vitorias = 10
 derrotas = 2
 let nivel
+console.log("O herói tem saldo de " + saldo + " e está no nível " + nivel + "!");
+        
+function calcularSaldo (vitorias, derrotas) {
 
-function calcularSaldo () {
-console.log(vitorias - derrotas)
-}
-
-function concatenar () {
-switch (true) {
-    case calcularSaldo() < 10:
+   let saldo = vitorias - derrotas;
+   
+   switch (true) {
+    case vitorias < 10:
         nivel = "Ferro";
-        console.log("O herói tem saldo de " + calcularSaldo() + " e está no nível " + nivel + "!");
         break;
-    case calcularSaldo() <= 20:
+    case vitorias <= 20:
         nivel = "Bronze";
-        console.log("O herói tem saldo de " + calcularSaldo() + " e está no nível " + nivel + "!");
         break;
-    case calcularSaldo() <= 50:
+    case vitorias <= 50:
         nivel = "Prata";
-        console.log("O herói tem saldo de " + calcularSaldo() + " e está no nível " + nivel + "!");
         break;
-    case calcularSaldo() <= 80:
+    case vitorias <= 80:
         nivel = "Ouro";
-        console.log("O herói tem saldo de " + calcularSaldo() + " e está no nível " + nivel + "!");
         break;
-    case calcularSaldo() <= 90:
+    case vitorias <= 90:
         nivel = "Diamante";
-        console.log("O herói tem saldo de " + calcularSaldo() + " e está no nível " + nivel + "!");
         break;
-    case calcularSaldo() <= 100:
+    case vitorias <= 100:
         nivel = "Lendário";
-        console.log("O herói tem saldo de " + calcularSaldo() + " e está no nível " + nivel + "!");
         break;
     default:
         nivel = "Imortal";
-        console.log("O herói tem saldo de " + calcularSaldo() + " e está no nível " + nivel + "!");
         break;
- }
+   }
 }
-concatenar()
+return saldo()
